07b376129c3a402c97d73029ba07ead43f01b39e
Jason <jbpulis@yahoo.com>
2018-04-21 01:47:37 -0500

    man this was bad record keeping
    updated index, script, and style... a lot.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-	var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
@@ -30,5 +30,5 @@ app.controller('BlogController', function($scope) {
 });
 
 app.controller('AboutController', function($scope) {
-  $scope.message = 'AboutController';
+  $scope.message = 'Hello from AboutController';
 });

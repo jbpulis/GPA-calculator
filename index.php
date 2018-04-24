29babb3fb4e9cc44c2bd3b5baa22720e0816d118
Jason <jbpulis@yahoo.com>
2018-04-24 00:35:20 -0500

    index cleanup
    just made to html easier to read

--- index.php
+++ index.php
@@ -1,60 +1,54 @@
 <!doctype html>
 <html ng-app="GPAapp">
-  <head>
-  <title>MyGPA-Calculator</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-route.min.js"></script>
-    <link rel="stylesheet" ng-href="style.css">
-  </head>
-  <body>
-    
-<div class="main">
-    <div class="navbar">
-        <nav id="nav01">
-            <ul id="menu">
-                <li>
-                    <a href="#/">Main</a>
-                </li>
-                <li>
-                    <a href="#/edit">Edit</a>
-                </li>
-                <li>
-                    <a href="#/new">New</a>
-                </li>
-            </ul>
-        </nav>
-        <nav id="nav02">
-            <ul id="menu2">
-                <li>
-                <a href="#/">MyGPA</a>
-                </li>
-            </ul>
-        </nav>
-        <nav id="nav03">
-            <ul id="menu3">
-                <li>
-                    <a href="#/login" onclick="window.location.href='#/login'">Log In</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-
-    
-    <div id="center">
-        <div id="left-stripe"></div>
-        <div id="right-stripe"></div>
-        <div id="content"> 
-            <div ng-view></div>
+    <head>
+        <title>MyGPA-Calculator</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-route.min.js"></script>
+        <link rel="stylesheet" ng-href="style.css">
+    </head>
+    <body>
+        <div class="main">
+            <div class="navbar">
+                <nav id="nav01">
+                    <ul id="menu">
+                        <li>
+                            <a href="#/">Main</a>
+                        </li>
+                        <li>
+                            <a href="#/edit">Edit</a>
+                        </li>
+                        <li>
+                            <a href="#/new">New</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav id="nav02">
+                    <ul id="menu2">
+                        <li>
+                            <a href="#/">MyGPA</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav id="nav03">
+                    <ul id="menu3">
+                        <li>
+                            <a href="#/login" onclick="window.location.href='#/login'">Log In</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="center">
+                <div id="left-stripe"></div>
+                <div id="right-stripe"></div>
+                <div id="content">
+                    <div ng-view></div>
+                </div>
+                
+            </div>
+            <script src="model_controller_script.js"></script>
         </div>
-        
-    </div>
-<script src="model_controller_script.js"></script>
-</div>
-    
-
-
-    <footer id="foot01">
-        <p>  <?php echo date("Y"); ?> GPA-calculator. All rights reserved.</p>
-    </footer>
-  </body>
+        <footer id="foot01">
+            <p>  <?php echo date("Y"); ?> GPA-calculator. All rights reserved.</p>
+        </footer>
+    </body>
 </html>
